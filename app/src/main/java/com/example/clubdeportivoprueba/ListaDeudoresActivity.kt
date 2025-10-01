542c4b4ea89e476529e64938d16bf1b4be316660
leo_R37 <leandro_rocha93@hotmail.com>
2025-09-30 21:01:13 -0300

    feat: Implementa la pantalla de Lista de Deudores
    Se desarrolla la interfaz de usuario para la pantalla "Lista de Deudores", que muestra una lista de deudores con su información y permite la selección para un cobro posterior.
    
    Cambios realizados:
    - Se rediseña el layout `activity_lista_deudores.xml` para incluir un campo de búsqueda, una lista de deudores con checkboxes y un botón para cobrar.
    - Se agregan nuevos strings en `strings.xml` para la información de los deudores y el campo de búsqueda.
    - Se implementa la lógica en `ListaDeudoresActivity.kt` para manejar el botón de volver y la navegación hacia `CobrarActivity` al presionar el botón "Cobrar".

--- app/src/main/java/com/example/clubdeportivoprueba/ListaDeudoresActivity.kt
+++ app/src/main/java/com/example/clubdeportivoprueba/ListaDeudoresActivity.kt
@@ -1,6 +1,9 @@
 package com.example.clubdeportivoprueba
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,17 @@ class ListaDeudoresActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnVolver = findViewById<ImageButton>(R.id.btnVolver)
+        btnVolver.setOnClickListener {
+            finish()
+        }
+
+        val btnCobrar = findViewById<Button>(R.id.btnCobrar)
+        btnCobrar.setOnClickListener {
+            val intent = Intent(this, CobrarActivity::class.java)
+            startActivity(intent)
         }
     }
 }
